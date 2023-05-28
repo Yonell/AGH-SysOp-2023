9dca8c2d4ef0e85c70236339a4adba888d365752
Filip Piskorski <fpfpfp2002@gmail.com>
2023-05-28 17:51:25 +0200

    Some segfault

---
+++ PiskorskiFilip/cw10/TCP/client.cpp
@@ -0,0 +1,235 @@
+#include <iostream>
+#include "./constants.h"
+#include <ctime>
+#include <cstdlib>
+#include <cstring>
+#include <signal.h>
+#include <pwd.h>
+#include <arpa/inet.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+
+int socket_fd;
+int client_id;
+std::string name;
+bool local_or_inet;
+int server_port;
+in_addr server_address;
+std::string path;
+
+void send_stop_to_server() {
+    my_msgbuf_stop_to_server buf;
+    buf.type = MSG_STOP;
+    buf.data = client_id;
+    send(socket_fd, (char*)&buf, sizeof(buf), 0);
+    shutdown(socket_fd, SHUT_RDWR);
+    close(socket_fd);
+}
+
+void sigint_handler(int sig){
+    send_stop_to_server();
+    exit(0);
+}
+
+void send_init_to_server(){
+    struct my_msgbuf_init_to_server buf{MSG_INIT};
+    strcpy(buf.data, name.c_str());
+    int result = send(socket_fd, (char*)&buf, sizeof(buf), 0);
+    if(result == -1){
+        std::cout << "CLIENT: Failed to contact the server" << "\n";
+        std::cout << "CLIENT: Reason: " << strerror(errno) << "\n";
+    }
+}
+
+void receive_init_from_server(){
+    struct my_msgbuf_init_to_client buf;
+    while(recv(socket_fd, (char*)&buf, 20000, 0)==-1);
+    if(buf.type != MSG_INIT){
+        std::cout << "CLIENT: Wrong mssg type: " << buf.type << "\n";
+        std::cout << "CLIENT: Expected: " << MSG_INIT << "\n";
+        exit(1);
+    }
+    client_id = buf.data;
+}
+
+void print_list_from_server(){
+    my_msgbuf_list_to_server buf;
+    buf.type = MSG_LIST;
+    buf.data = client_id;
+    std::cout << "CLIENT: Sending list request to server\n";
+    send(socket_fd, (char*)&buf, sizeof(buf), 0);
+    std::cout << "CLIENT: List request sent\n";
+    std::cout << "CLIENT: Waiting for list from server\n";
+    my_msgbuf_list_to_client buf2;
+    recv(socket_fd, &buf2, sizeof(buf2), 0);
+    std::cout << "CLIENT: List received\n";
+    std::cout << "CLIENT: List of clients: " << "\n";
+    for(int i = 0; i < buf2.size; i++){
+        std::cout << "CLIENT: " << buf2.data[i] << "\n";
+    }
+    std::cout << "CLIENT: End of the list\n";
+}
+
+void send_2one_message(){
+    my_msgbuf_2one_to_server buf;
+    buf.type = MSG_2ONE;
+    buf.source_id = client_id;
+    std::cout << "CLIENT: Enter the id of the client you want to send a message to: ";
+    int dest_id;
+    std::cin >> dest_id;
+    buf.destination_id = dest_id;
+    std::cout << "CLIENT: Enter the message: ";
+    std::string string_buffer;
+    std::cin.ignore(2, '\n');
+    std::getline(std::cin, string_buffer);
+    memcpy(buf.message, string_buffer.c_str(), string_buffer.size()+1);
+    buf.message[string_buffer.size()] = 0;
+    buf.size = strlen(buf.message);
+    send(socket_fd, (char*)&buf, sizeof(buf), 0);
+}
+
+void send_2all_message(){
+    my_msgbuf_2all_to_server buf;
+    buf.type = MSG_2ALL;
+    buf.source_id = client_id;
+    std::cout << "CLIENT: Enter the message: ";
+    std::string string_buffer;
+    std::cin.ignore(2, '\n');
+    std::getline(std::cin, string_buffer);
+    memcpy(buf.message, string_buffer.c_str(), string_buffer.size()+1);
+    buf.message[string_buffer.size()] = 0;
+    buf.size = strlen(buf.message);
+    send(socket_fd, (char*)&buf, sizeof(buf), 0);
+}
+
+void read_queue() {
+    my_msgbuf_2one_to_server buf;
+    unsigned int* type = new unsigned int;
+    timespec* timeout = new timespec;
+    timeout->tv_sec = 0;
+    while(recv(socket_fd, (char*)&buf, 20000, 0) > 0) {
+        *type = buf.type;
+        switch (*type) {
+            case MSG_2ONE: {
+                std::cout << "CLIENT: Message received from " << buf.source_id << ": " << buf.message << "\n";
+                break;
+            }
+            case MSG_2ALL: {
+                my_msgbuf_2all_to_client buf2 = *(my_msgbuf_2all_to_client *) &buf;
+                std::cout << "CLIENT: Message received from " << buf2.source_id << ": " << buf2.message << "\n";
+                break;
+            }
+            case MSG_LIST: {
+                my_msgbuf_list_to_client buf3 = *(my_msgbuf_list_to_client *) &buf;
+                std::cout << "CLIENT: List of clients: " << "\n";
+                for (int i = 0; i < buf3.size; i++) {
+                    std::cout << "CLIENT: " << buf3.data[i] << "\n";
+                }
+                std::cout << "CLIENT: End of the list\n";
+                break;
+            }
+            case MSG_STOP: {
+                std::cout << "CLIENT: Server stopped\n";
+                raise(SIGINT);
+                break;
+            }
+        }
+    }
+}
+
+int main(int argc, char* argv[]){
+    if(argc != 4 && argc != 5){
+        std::cout << "CLIENT: Wrong number of arguments! Required: 3/4, got: " << argc - 1 << "\n";
+        exit(0);
+    }
+    name = std::string(argv[1]);
+    if(strcmp(argv[2], "local") == 0){
+        local_or_inet = 0;
+        if(argc == 4){
+            path = std::string(argv[3]);
+        } else {
+            std::cout << "CLIENT: Wrong number of arguments! Required: 3, got: " << argc - 1 << "\n";
+            exit(0);
+        }
+    } else if(strcmp(argv[2], "inet") == 0){
+        local_or_inet = 1;
+        if(argc == 5){
+            inet_aton(argv[3], &server_address);
+            server_port = atoi(argv[4]);
+        } else {
+            std::cout << "CLIENT: Wrong number of arguments! Required: 4, got: " << argc - 1 << "\n";
+            exit(0);
+        }
+    } else {
+        std::cout << "CLIENT: Wrong type of connection! Required: local/inet, got: " << argv[2] << "\n";
+        exit(0);
+    }
+
+
+    struct sigaction sigaction1;
+    sigaction1.sa_handler = sigint_handler;
+    sigaction(SIGINT, &sigaction1, nullptr);
+
+    std::cout << "CLIENT: " << "Client started, PID:" << getpid() << "\n";
+
+    if(!local_or_inet){
+        if((socket_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1){
+            std::cout << "CLIENT: Error while creating socket\n";
+            std::cout << "CLIENT: Reason: " << strerror(errno) << "\n";
+            exit(0);
+        }
+        sockaddr_un serv_address;
+        serv_address.sun_family = AF_UNIX;
+        strcpy(serv_address.sun_path, path.c_str());
+        if(connect(socket_fd, (sockaddr*)&serv_address, sizeof(serv_address)) == -1){
+            std::cout << "CLIENT: Error while connecting to the server\n";
+            std::cout << "CLIENT: Reason: " << strerror(errno) << "\n";
+            exit(0);
+        }
+    } else {
+        if((socket_fd = socket(AF_INET, SOCK_STREAM, 0))==-1){
+            std::cout << "CLIENT: Error while creating socket\n";
+            std::cout << "CLIENT: Reason: " << strerror(errno) << "\n";
+            exit(0);
+        }
+        sockaddr_in serv_address;
+        serv_address.sin_family = AF_INET;
+        serv_address.sin_addr = server_address;
+        serv_address.sin_port = htons(server_port);
+        if(connect(socket_fd, (sockaddr*)&serv_address, sizeof(serv_address)) == -1){
+            std::cout << "CLIENT: Error while connecting to the server\n";
+            std::cout << "CLIENT: Reason: " << strerror(errno) << "\n";
+            exit(0);
+        }
+
+    }
+
+    std::cout << "CLIENT: Sending init signal to the server..." << "\n";
+    send_init_to_server();
+    std::cout << "CLIENT: Waiting for the server response..." << "\n";
+    receive_init_from_server();
+    std::cout << "CLIENT: Client id received: " << client_id << "\n";
+
+    std::cout << "CLIENT: Starting the chat..." << "\n";
+    std::string command;
+    std::cout << "CLIENT: Enter a command: "; std::cin >> command;
+    while(command != "stop"){
+        if(command == "list"){
+            print_list_from_server();
+        } else if(command == "2all"){
+            send_2all_message();
+        } else if(command == "2one"){
+            send_2one_message();
+        } else if(command == "read_queue"){
+            read_queue();
+        } else {
+            std::cout << "CLIENT: Unknown command. Try again..." << "\n";
+        }
+        std::cout << "CLIENT: Enter a command: "; std::cin >> command;
+    }
+    std::cout << "CLIENT: Sending stop signal to the server..." << "\n";
+    send_stop_to_server();
+    std::cout << "CLIENT: Exiting..." << "\n";
+    return 0;
+}
---
+++ PiskorskiFilip/cw10/TCP/server.cpp
@@ -0,0 +1,257 @@
+#include "./constants.h"
+#include <cerrno>
+#include <ctime>
+#include <chrono>
+#include <map>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <signal.h>
+#include <cstring>
+#include <fstream>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <thread>
+#include <arpa/inet.h>
+#include <sys/un.h>
+
+
+struct client {
+    int id;
+    int client_socket_fd;
+};
+std::map<int, client> clients;
+int port;
+std::string UNIX_socket_path;
+int local_socket_fd;
+int inet4_socket_fd;
+int epollfd;
+std::ofstream log_file;
+struct epoll_event event, events[2];
+
+std::string return_current_time_and_date()
+{
+    auto now = std::chrono::system_clock::now();
+    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+
+    std::stringstream ss;
+    ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X");
+    return ss.str();
+}
+
+std::vector<int> get_keys(const std::map<int, client>& clients){
+    std::vector<int> result;
+    result.reserve(clients.size());
+    for (auto & client : clients) {
+        result.push_back(client.first);
+    }
+    return result;
+}
+
+void sigint_handler(int sig){
+    std::cout << "SERVER: " << return_current_time_and_date() << " SIGINT signal received! Exiting...\n";
+    for(long unsigned int i=0; i<clients.size(); i++){
+        struct my_msgbuf_stop_to_client buf{MSG_STOP};
+        send(clients[i].client_socket_fd, (char *)(&buf), sizeof(buf), 0);
+        shutdown(clients[i].client_socket_fd, SHUT_RDWR);
+        close(clients[i].client_socket_fd);
+    }
+    shutdown(local_socket_fd, SHUT_RDWR);
+    shutdown(inet4_socket_fd, SHUT_RDWR);
+    close(local_socket_fd);
+    close(inet4_socket_fd);
+    unlink(UNIX_socket_path.c_str());
+    close(epollfd);
+    std::cout << "SERVER: " << return_current_time_and_date() << " Server stopped\n";
+    exit(0);
+}
+
+void listening_thread_function(){
+    std::cout << "SERVER: " << return_current_time_and_date() << " Server is listening on port " << port << " and on UNIX socket " << UNIX_socket_path << "\n";
+
+    struct my_msgbuf_2one_to_server buf;
+    while(1){
+        int nfds = epoll_wait(epollfd, events, 64, -1);
+        if (nfds == -1) {
+            std::cout << "SERVER: " << return_current_time_and_date() << " Error while waiting for events\n";
+            exit(1);
+        }
+        for(int i = 0; i<nfds; ++i) {
+            if(events[i].data.fd == local_socket_fd || events[i].data.fd == inet4_socket_fd){
+                std::cout << "SERVER: " << return_current_time_and_date() << " New connection on socket " << events[i].data.fd << "\n";
+                int client_socket_fd = accept(events[i].data.fd, nullptr, nullptr);
+                if(client_socket_fd == -1){
+                    std::cout << "SERVER: " << return_current_time_and_date() << " Error while accepting new connection\n";
+                    std::cout << "SERVER: Reason: " << strerror(errno) << "\n";
+                    exit(1);
+                }
+                std::cout << "SERVER: " << return_current_time_and_date() << " Client accepted\n";
+                event.events = EPOLLIN;
+                event.data.fd = client_socket_fd;
+                if(epoll_ctl(epollfd, EPOLL_CTL_ADD, client_socket_fd, &event) == -1){
+                    std::cout << "SERVER: " << return_current_time_and_date() << " Error while adding client socket to epoll\n";
+                    exit(1);
+                }
+                std::cout << "SERVER: " << return_current_time_and_date() << " Client socket added to epoll\n";
+                int client_id;
+                std::vector<int> keys = get_keys(clients);
+                if (keys.size() == 0)
+                    client_id = 0;
+                else
+                    client_id = (*std::max_element(keys.begin(), keys.end())) + 1;
+                struct client new_client{client_id, client_socket_fd};
+                clients.insert(std::pair<int, client>(client_id, new_client));
+                std::cout << "SERVER: " << return_current_time_and_date() << " Client added to clients map\n";
+                struct my_msgbuf_init_to_client* buf3 = new my_msgbuf_init_to_client{MSG_INIT, client_id};
+                send(client_socket_fd, (char *)(buf3), sizeof(buf3), 0);
+                std::cout << "SERVER: " << return_current_time_and_date() << " MSG_INIT sent to client\n";
+            }
+            else{
+                std::cout << "LOL";
+                buf = *((my_msgbuf_2one_to_server*)events[i].data.ptr);
+                unsigned int type = buf.type;
+                switch(type) {
+                    case MSG_STOP: {
+                        my_msgbuf_stop_to_server buf3 = *((my_msgbuf_stop_to_server*) &buf);
+                        shutdown(buf3.data, SHUT_RDWR);
+                        close(buf3.data);
+                        std::cout << "SERVER: " << return_current_time_and_date() << " Client " << buf3.data
+                                  << " disconnected." << "\n";
+                        clients.erase(buf.source_id);
+                        break;
+                    }
+                    case MSG_LIST: {
+                        my_msgbuf_list_to_server buf4 = *((my_msgbuf_list_to_server*) &buf);
+                        std::cout << "SERVER: " << return_current_time_and_date() << " Client " << buf4.data
+                                  << " requested list of clients.\n";
+                        std::vector<int> keys = get_keys(clients);
+                        struct my_msgbuf_list_to_client buf2{MSG_LIST, (int)keys.size()};
+                        memcpy(buf2.data, keys.data(), keys.size() * sizeof(int));
+                        send(clients[buf4.data].client_socket_fd, (char *)&buf2, sizeof(buf2), 0);
+                        break;
+                    }
+                    case MSG_2ALL: {
+                        my_msgbuf_2all_to_server buf5 = *((my_msgbuf_2all_to_server*) &buf);
+                        std::cout << "SERVER: " << return_current_time_and_date() << " Message received from client " << buf5.source_id
+                                  << " to all clients: " << buf5.message << "\n";
+                        for (long unsigned int i = 0; i < clients.size(); i++) {
+                            struct my_msgbuf_2all_to_client buf2{MSG_2ALL, buf5.source_id, buf5.size};
+                            strcpy(buf2.message, buf5.message);
+                            send(clients[i].client_socket_fd, (char *)&buf2, sizeof(buf2), 0);
+                        }
+                        break;
+                    }
+                    case MSG_2ONE:{
+                        my_msgbuf_2one_to_server buf2 = *((my_msgbuf_2one_to_server*) &buf);
+                        std::cout << "SERVER: " << return_current_time_and_date() << " Message received from client " << buf2.source_id
+                                  << " to client " << buf2.destination_id << ": " << buf2.message << "\n";
+                        struct my_msgbuf_2one_to_client buf3{MSG_2ONE, buf2.source_id, buf2.size};
+                        strcpy(buf3.message, buf2.message);
+                        send(clients[buf2.destination_id].client_socket_fd, (char *)&buf3, sizeof(buf3), 0);
+                        break;
+                    }
+                    default:{
+                        std::cout << "SERVER: " << return_current_time_and_date() << "Received uknown message type: " << type << "\n";
+                        std::cout << "SERVER: Skipping...\n";
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main(int argc, char* argv[]){
+    if(argc != 3){
+        std::cout << "SERVER: Wrong number of arguments! Required: 2, got: " << argc - 1 << "\n";
+        exit(0);
+    }
+    port = atoi(argv[1]);
+    UNIX_socket_path = std::string(argv[2]);
+
+    struct sigaction sigaction1;
+    sigaction1.sa_handler = sigint_handler;
+    sigaction(SIGINT, &sigaction1, nullptr);
+
+    std::cout << "SERVER: " << return_current_time_and_date() << "Server started, PID:" << getpid() << "\n";
+
+    local_socket_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if(local_socket_fd == -1){
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Failed to create a local socket!" << "\n";
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Reason: " << strerror(errno) << "\n";
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Exiting..." << "\n";
+        exit(0);
+    }
+    std::cout << "SERVER: " << return_current_time_and_date() << " Local socket created successfully. Socket id: " << local_socket_fd << "\n";
+
+    inet4_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(inet4_socket_fd == -1){
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Failed to create an IPv4 socket!" << "\n";
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Reason: " << strerror(errno) << "\n";
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Exiting..." << "\n";
+        exit(0);
+    }
+    std::cout << "SERVER: " << return_current_time_and_date() << " IPv4 socket created successfully. Socket id: " << inet4_socket_fd << "\n";
+
+    struct sockaddr_un local_address;
+    memset(&local_address, 0, sizeof(local_address));
+    local_address.sun_family = AF_UNIX;
+    strcpy(local_address.sun_path, UNIX_socket_path.c_str());
+    if(bind(local_socket_fd, (struct sockaddr *) &local_address, sizeof(local_address)) == -1){
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Failed to bind a local socket!" << "\n";
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Reason: " << strerror(errno) << "\n";
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Exiting..." << "\n";
+        exit(0);
+    }
+    std::cout << "SERVER: " << return_current_time_and_date() << " Local socket bound successfully.\n";
+
+    struct sockaddr_in inet4_address;
+    inet4_address.sin_family = AF_INET;
+    inet4_address.sin_port = htons(port);
+    inet4_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    if(bind(inet4_socket_fd, (struct sockaddr *) &inet4_address, sizeof(inet4_address)) == -1){
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Failed to bind an IPv4 socket!" << "\n";
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Reason: " << strerror(errno) << "\n";
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Exiting..." << "\n";
+        exit(0);
+    }
+    std::cout << "SERVER: " << return_current_time_and_date() << " IPv4 socket bound successfully.\n";
+
+    if(listen(local_socket_fd, 10) == -1){
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Failed to listen on a local socket!" << "\n";
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Reason: " << strerror(errno) << "\n";
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Exiting..." << "\n";
+        exit(0);
+    }
+    if(listen(inet4_socket_fd, 10) == -1){
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Failed to listen on a local socket!" << "\n";
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Reason: " << strerror(errno) << "\n";
+        std::cout << "SERVER: " << return_current_time_and_date() << " ERROR Exiting..." << "\n";
+        exit(0);
+    }
+    epollfd = epoll_create1(0);
+    if(epollfd == -1){
+        std::cout << "SERVER: " << return_current_time_and_date() << " Error while creating epoll\n";
+        exit(1);
+    }
+
+    event.events = EPOLLIN;
+    event.data.fd = local_socket_fd;
+    if(epoll_ctl(epollfd, EPOLL_CTL_ADD, local_socket_fd, &event) == -1){
+        std::cout << "SERVER: " << return_current_time_and_date() << " Error while adding local socket to epoll\n";
+        exit(1);
+    }
+    event.data.fd = inet4_socket_fd;
+    if(epoll_ctl(epollfd, EPOLL_CTL_ADD, inet4_socket_fd, &event) == -1){
+        std::cout << "SERVER: " << return_current_time_and_date() << " Error while adding inet4 socket to epoll\n";
+        exit(1);
+    }
+    std::cout << "SERVER: " << return_current_time_and_date() << " epoll successfully created " << "\n";
+
+    std::cout << "SERVER: " << return_current_time_and_date() << " Starting a listening thread...\n";
+    std::thread listening_thread(listening_thread_function);
+
+    listening_thread.join();
+
+    exit(0);
+}
